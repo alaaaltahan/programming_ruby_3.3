f4f4d8ee4950e9113b432e1b3a3525ebba5d5ef7
alaa ahmad altahan <alaath777@gmail.com>
2025-02-27 07:18:17 +0300

    first hello world file

---
+++ ch2_ruby_new/hello1.rb
@@ -0,0 +1,8 @@
+def say_hello_goodbye name,lastname
+  result = "Idon't know why you say goodbye, " + name + " " + lastname + ", I say hello"
+  return result
+end  
+
+# call the method
+puts say_hello_goodbye("john","altahan")
+puts say_hello_goodbye("paul","altahan")
