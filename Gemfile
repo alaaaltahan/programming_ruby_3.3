01819909ca23d1f57c9026de742407b6ff72b79f
alaa ahmad altahan <alaath777@gmail.com>
2025-02-28 07:03:53 +0300

    just how to handle ruby lsp it is out of book but it is essential extension for ruby

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# Gemfile
+source 'https://rubygems.org'
+
+ruby '3.3.5'
+
+# Ruby LSP
+gem 'rubocop'
+gem 'ruby-lsp'
--- ch2_ruby_new/hello1.rb
+++ ch2_ruby_new/hello1.rb
@@ -1,8 +1,9 @@
-def say_hello_goodbye name,lastname
-  result = "Idon't know why you say goodbye, " + name + " " + lastname + ", I say hello"
-  return result
-end  
+# frozen_string_literal: true
+
+def say_hello_goodbye(name, last_name)
+  "I don't know why you say goodbye, #{name} #{last_name}, I say hello"
+end
 
 # call the method
-puts say_hello_goodbye("john","altahan")
-puts say_hello_goodbye("paul","altahan")
+puts say_hello_goodbye('john', 'altahan')
+puts say_hello_goodbye('paul', 'altahan')
