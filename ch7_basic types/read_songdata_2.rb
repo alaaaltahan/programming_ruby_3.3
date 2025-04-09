41367c7c1e729310ee05f4c8299f2fabf65459b5
alaa ahmad altahan <alaath777@gmail.com>
2025-04-09 06:28:42 +0300

    just use squeeze method in string class to remove the extra spaces in the name of the song

--- ch7_basic types/read_songdata_2.rb
+++ ch7_basic types/read_songdata_2.rb
@@ -3,6 +3,6 @@ Song = Struct.new(:title, :name, :length)
 songs = File.readlines("songdata.txt", chomp: true).map do 
   |line|
   _file, length, name, title = line.chomp.split(/\s*\|\s*/)
-  Song.new(title, name, length)
+  Song.new(title, name.squeeze(" "), length)
 end
 puts songs[1]
