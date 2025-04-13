2c6f1bde4b8f0e3219058f4a5ec6264513ca798f
alaa ahmad altahan <alaath777@gmail.com>
2025-04-13 08:47:26 +0300

    study of range

---
+++ ch7_basic types/range_case.rb
@@ -0,0 +1,14 @@
+car_age = gets.to_f
+
+case car_age
+when 0...1
+  puts "Mmm.. new car smell"
+when 1...3
+  puts "Nice and new"
+when 3...10
+  puts "Reliable nut slightly dinged"
+when 10...30
+  puts "Clunker"
+else
+  puts "Vintage gem"    
+end
